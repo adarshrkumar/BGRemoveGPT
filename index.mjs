aaf2124b7ef6a17c9c19453817975507850a74c5
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-01-26 16:40:47 -0800

    update index.mjs

--- index.mjs
+++ index.mjs
@@ -85,7 +85,7 @@ async function getExecutionUntilFound(id, res, i) {
   if (!result.content.status) result.content.status = {name: 'error', description: 'No execution found OR error on code.'};
 
   if (i > 60) {
-    res.status(408).send(useUploadHTML(408, `Session Timeout, please try again later.`))
+    res.status(408).send(useErrorTemplate(408, `Session Timeout, please try again later.`))
     return
   }
 
@@ -142,7 +142,7 @@ app.get('/getExecution', async (req, res) => {
 ['upload', 'uploadRemove'].forEach((path) => {
 
     app.get(`/${path}`, (req, res) => {
-      res.status(200).send(useErrorTemplate(path))
+      res.status(200).send(useUploadHTML(path))
     });
     
 
